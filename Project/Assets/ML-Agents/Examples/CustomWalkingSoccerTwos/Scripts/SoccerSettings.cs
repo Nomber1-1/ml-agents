2e21a42afa3ecf6dfbce2105eab4dbfc7b53281e
Patrick Zakaria <73553056+Nomber1-1@users.noreply.github.com>
2025-11-23 22:37:12 -0500

    CustomWalkerSoccer Scene

---
+++ Project/Assets/ML-Agents/Examples/CustomWalkingSoccerTwos/Scripts/SoccerSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SoccerSettings2 : MonoBehaviour
+{
+    public Material purpleMaterial;
+    public Material blueMaterial;
+    public bool randomizePlayersTeamForTraining = true;
+    public float agentRunSpeed;
+}
